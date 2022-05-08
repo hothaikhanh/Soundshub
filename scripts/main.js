dd3e3b3ede6044dfe710bbcb00443d64aa91eb2f
Hồ Thái Khanh <hothaikhanh@gmail.com>
2022-05-08 23:53:22 +0700

    complete JSON file

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
+//fetch JSON data from directory
+var songLibrary = [0,1,2];
+
+
+//fetch sth.....
+
+
+
+//create an array containing the paths for every songs
+
+// var songsNumber = songLibrary.length
+
+
+// const songList = $(".song-list")
+
+// const app = {
+//     songs: [
+//         songLibrary[0],
+//         songLibrary[1],
+//         songLibrary[2],
+        
+//     ]
+
+
+
+
+
+// }
+
+
+
+    // songList.innerHTML += `
+    //     <li class="song-item">
+    //         <audio class = "song-item__mp3" data-index ="${i+1}" src="${paths[i]}"></audio>
+    //         <span class="song-item__index"> ${i+1} </span>
+    //         <img src="" alt="" class="song-item__art">    
+    //         <div class="song-item__title"></div>
+    //         <div class="song-item__artist"></div>
+    //         <div class="song-item__album"></div>
+    //         <div class="song-item__genre"></div>
+    //         <span class="song-item__duration"></span>
+    //     </li>`
+
+        
+
+
+
+
